74243c4128526b266b374a787bb53f24cb24cae8
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2023-06-01 01:12:14 +0700

    add request rest

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,5 +1,5 @@
 import Users from "../models/user_model.js";
-
+import bcrypt from "bcrypt";
 export const getUsers = async(req, res) => {
     try {
         const users = await Users.findAll();
@@ -7,4 +7,9 @@ export const getUsers = async(req, res) => {
     } catch (error) {
         console.log(error)
     }
+}
+
+export const register = async(req, res) => {
+    const {name, email, password, confPassword} = req.body;
+    if (password !== confPassword) return res.status(400).json
 }
