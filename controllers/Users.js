4f1a4052abc2db0ae973b8508a6e904d9155cadf
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2023-06-01 20:32:10 +0700

    add update and delete method

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,5 +1,5 @@
 import User from "../models/UserModel.js";
-import argon2 from "argon2";
+import argon2, {hash} from "argon2";
 
 export const getUsers = async(req, res) => {
     try {
@@ -45,9 +45,53 @@ export const createUser = async(req, res) =>{
 
 
 export const updateUser = async (req, res) =>{
-
+    const user = await User.findOne({
+        where: {
+            uuid: req.params.id
+        }
+    });
+    if (!user) return res.status(404).json({msg:"User tidak ditemukan!"})
+    const {name, email, password, confPassword, role} = req.body;
+    let hashPassword;
+    if (password === "" || password === null){
+        hashPassword = user.password
+    }else {
+        hashPassword = await argon2.hash(password);
+    }
+    if(password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak cocok"});
+    try {
+        await User.update({
+            name: name,
+            email: email,
+            password: hashPassword,
+            role: role
+        },{
+            where:{
+                id: user.id
+            }
+        });
+        res.status(200).json({msg: "User telah diupdate!"});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
 }
 
 export const deleteUser = async (req, res) =>{
+    const user = await User.findOne({
+        where: {
+            uuid: req.params.id
+        }
+    });
+    if (!user) return res.status(404).json({msg:"User tidak ditemukan!"})
+    try {
+        await User.destroy({
+            where:{
+                id: user.id
+            }
+        });
+        res.status(200).json({msg: "User telah dihapus!"});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
 
 }
